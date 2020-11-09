f3a7c0ce5c80438181cbb0352e1f78066a6bd860
Kevin Ou <kevin341@gmail.com>
2020-11-09 00:24:33 -0500

    Refactor sleep to use setTimeout

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const reddit = new snoowrap({
 });
 
 var array = [];
+var waitTime = 0;
 
 reddit.getSubmission('jpsm1a').expandReplies().then(com => {
 	sortComments(com.comments)
@@ -19,22 +20,64 @@ reddit.getSubmission('jpsm1a').expandReplies().then(com => {
 
 function sortComments(comments) {
 	comments.sort((a, b) => (a.created_utc - b.created_utc))
-	// comments.forEach(com => {
-	// 	console.log(com.body)
-	// });
-
-	for (var i = 0; i < comments.length - 1; i++) {
-		if (comments[i].body != '[deleted]') {
-			const date = new Date(comments[i].created_utc * 1000)
-			console.log("From: " + comments[i].author.name + " at " + date)
-			console.log(comments[i].body)
-			console.log()
-			console.log("======================================================================")
-			console.log()
-			const waitTime = comments[i + 1].created_utc - comments[i].created_utc
-			sleep(waitTime * 1000)
-		}
+	for (var i = 0; i < comments.length - 1; i++) {		// last comment will not be displayed
+		displayComment(comments, i)
 	}
+}
+
+function displayComment(comments, i) {
+	if (i > 0) {
+		waitTime += (comments[i].created_utc - comments[i - 1].created_utc) * 1000
+		setTimeout(function () {
+			printComment(comments, i);
+		}, waitTime)
+	} else {
+		printComment(comments, i);
+	}
+
+}
+
+function printComment(comments, i) {
+	// if (comments[i].parent_id.substring(0, 2) == "t3") {
+	if (comments[i].body != '[deleted]') {
+		const date = new Date(comments[i].created_utc * 1000)
+		if (comments[i].author_flair_text == null) {
+			console.log(`${comments[i].author.name} | ${date}`)
+
+		} else {
+			console.log(`${comments[i].author.name} [${comments[i].author_flair_text}] | ${date}`)
+		}
+		console.log()
+		console.log(comments[i].body)
+		console.log()
+		console.log(`============================== [ i ===  ${i} ] ======================================`)
+		console.log()
+	}
+
+	// for (var i = 0; i < comments[i].replies.length - 1; i++) {			// last comment will not be displayed
+	// 	const waitTime = comments[i + 1].created_utc - comments[i].created_utc
+	// 	setTimeout(displayComment, waitTime, comments.replies, i)
+	// }
+	// } else if (comments[i].parent_id.substring(0, 2) == "t1") {
+	// 	if (comments[i].body != '[deleted]') {
+	// 		const date = new Date(comments[i].created_utc * 1000)
+	// 		if (comments[i].author_flair_text == null) {
+	// 			console.log(`${comments[i].author.name} | ${date}`)
+
+	// 		} else {
+	// 			console.log(`${comments[i].author.name} [${comments[i].author_flair_text}] | ${date}`)
+	// 		}
+	// 		console.log()
+	// 		console.log(comments[i].body)
+	// 		console.log()
+	// 		console.log(`     Replying to:`)
+	// 		var parentComment = comments.find(com => com.parent_id == comments[i].parent_id)
+	// 		console.log(`     Replying to:`)
+	// 		console.log(`     ${parentComment.body}`)
+	// 		console.log("======================================================================")
+	// 		console.log()
+	// 	}
+	// }
 }
 
 function sleep(milliseconds) {

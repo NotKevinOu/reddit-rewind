ec11636f8d3ceba5c0e7fae2b081d78ea0e09129
Kevin Ou <kevin341@gmail.com>
2020-11-08 22:21:17 -0500

    Sort comments by old

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 const http = require('http');
+var snoowrap = require('snoowrap');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+const reddit = new snoowrap({
+	userAgent: 'Reddit Rewind',
+	clientId: 'H7q-VgszU43xdw',
+	clientSecret: '5yC84krS2y03k9KEtKsgThDe7vIaQA',
+	refreshToken: '14546719-MzirzYqb_MJuYLgJGrvVuDMcTPDg9A'
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+var array = [];
+
+reddit.getSubmission('jpsm1a').expandReplies().then(com => {
+	sortComments(com.comments)
+})
+
+function sortComments(comments) {
+	comments.sort((a, b) => (a.created_utc - b.created_utc))
+	comments.forEach(com => {
+		console.log(com.body)
+	});
+}
+

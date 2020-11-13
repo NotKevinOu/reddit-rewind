c5a5a6c10fe3cd4a3077f481bd3563845725017b
Kevin Ou <kevin341@gmail.com>
2020-11-13 13:57:40 -0500

    Add reply comments

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
+/*
+	TODO:
+	- Start time
+	- Pause button
+	- HTML/CSS
+*/
+
 const http = require('http');
 var snoowrap = require('snoowrap');
 
@@ -11,22 +18,24 @@ const reddit = new snoowrap({
 	refreshToken: '14546719-MzirzYqb_MJuYLgJGrvVuDMcTPDg9A'
 });
 
-var array = [];
 var waitTime = 0;
 
-reddit.getSubmission('jpsm1a').expandReplies().then(com => {
+const eze = 'jpsm1a'
+const england = 'jt21oz'
+
+reddit.getSubmission(eze).expandReplies().then(com => {
 	sortComments(com.comments)
 })
 
 function sortComments(comments) {
 	comments.sort((a, b) => (a.created_utc - b.created_utc))
-	for (var i = 0; i < comments.length - 1; i++) {		// last comment will not be displayed
+	for (var i = 0; i < comments.length; i++) {
 		displayComment(comments, i)
 	}
 }
 
 function displayComment(comments, i) {
-	if (i > 0) {
+	if (i > 0 && i != comments.length) {
 		waitTime += (comments[i].created_utc - comments[i - 1].created_utc) * 1000
 		setTimeout(function () {
 			printComment(comments, i);
@@ -34,13 +43,24 @@ function displayComment(comments, i) {
 	} else {
 		printComment(comments, i);
 	}
-
 }
 
-function printComment(comments, i) {
-	// if (comments[i].parent_id.substring(0, 2) == "t3") {
+function displayReply(comment, replies, i) {
+	var waitReplyTime = 0;
+	if (i != replies.length) {
+		waitReplyTime += (replies[i].created_utc - comment.created_utc) * 1000 
+		setTimeout(function () {
+			printComment(replies, i);
+		}, waitReplyTime)
+	} else {
+		printComment(replies, i);
+	}
+}
+
+async function printComment(comments, i) {
 	if (comments[i].body != '[deleted]') {
 		const date = new Date(comments[i].created_utc * 1000)
+		// prints user flair if available 
 		if (comments[i].author_flair_text == null) {
 			console.log(`${comments[i].author.name} | ${date}`)
 
@@ -49,42 +69,37 @@ function printComment(comments, i) {
 		}
 		console.log()
 		console.log(comments[i].body)
+
+		if (comments[i].parent_id.substring(0, 2) == "t1") {
+			// if comment is a reply to another comment
+			console.log()
+			console.log(`     ====================`)
+			console.log()
+			console.log("     REPLYING TO:")
+			await reddit.getComment(comments[i].parent_id).fetch().then(parent => {
+				const date = new Date(parent.created_utc * 1000)
+				if (parent.author_flair_text == null) {
+					console.log(`${parent.author.name} | ${date}`)
+
+				} else {
+					console.log(`${parent.author.name} [${parent.author_flair_text}] | ${date}`)
+				}
+				console.log()
+				console.log(`${parent.body}`)
+				console.log()
+				console.log(`     ====================`)
+			})
+		}
 		console.log()
-		console.log(`============================== [ i ===  ${i} ] ======================================`)
+		console.log(`==========================================`)
 		console.log()
-	}
 
-	// for (var i = 0; i < comments[i].replies.length - 1; i++) {			// last comment will not be displayed
-	// 	const waitTime = comments[i + 1].created_utc - comments[i].created_utc
-	// 	setTimeout(displayComment, waitTime, comments.replies, i)
-	// }
-	// } else if (comments[i].parent_id.substring(0, 2) == "t1") {
-	// 	if (comments[i].body != '[deleted]') {
-	// 		const date = new Date(comments[i].created_utc * 1000)
-	// 		if (comments[i].author_flair_text == null) {
-	// 			console.log(`${comments[i].author.name} | ${date}`)
-
-	// 		} else {
-	// 			console.log(`${comments[i].author.name} [${comments[i].author_flair_text}] | ${date}`)
-	// 		}
-	// 		console.log()
-	// 		console.log(comments[i].body)
-	// 		console.log()
-	// 		console.log(`     Replying to:`)
-	// 		var parentComment = comments.find(com => com.parent_id == comments[i].parent_id)
-	// 		console.log(`     Replying to:`)
-	// 		console.log(`     ${parentComment.body}`)
-	// 		console.log("======================================================================")
-	// 		console.log()
-	// 	}
-	// }
-}
-
-function sleep(milliseconds) {
-	var start = new Date().getTime();
-	for (var i = 0; i < 1e7; i++) {
-		if ((new Date().getTime() - start) > milliseconds) {
-			break;
+		// process all replies to the comment, if any
+		if (comments[i].replies.length > 0) {
+			for (var j = 0; j < comments[i].replies.length; j++) {
+				displayReply(comments[i], comments[i].replies, j)
+			}
 		}
 	}
 }
+
